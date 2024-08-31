15b233c1bb712554971d7ccab526005daf6b7a1c
Nicholas Bergesen <nicholasb.za@github.com>
2024-08-31 16:48:13 +0900

    Add tests for reading domain name

--- main.go
+++ main.go
@@ -292,33 +292,41 @@ func byteArrayToString(data []byte) string {
 // readDomainName reads a domain name from the byte slice with support for message compression
 func readDomainName(data []byte, offset int) (string, int) {
 	var nameParts []string
-	for {
-		if offset >= len(data) {
-			break
-		}
+	fmt.Printf("readDomainName:offset:%d\n", offset)
+	if offset >= len(data) {
+		fmt.Printf("OOPS! offset exceeded data Length, looks like there's a bug\n")
+		return strings.Join(nameParts, "."), offset
+	}
 
-		length := int(data[offset])
-
-		// Check for the compression pointer (first two bits are 1s)
-		if length&0xC0 == 0xC0 {
-			// Read the offset of the compressed name
-			ptrOffset := int(binary.BigEndian.Uint16(data[offset:offset+2]) & 0x3FFF)
-			offset += 2
-
-			// Recursively read the domain name from the pointer offset
-			compressedName, _ := readDomainName(data, ptrOffset)
-			nameParts = append(nameParts, compressedName)
-			break
-		}
-
-		if length == 0 {
-			offset++
-			break
-		}
-
+	length := int(data[offset]) //gets int 0-255 value of byte
+	if length == 0 {
 		offset++
-		nameParts = append(nameParts, string(data[offset:offset+length]))
-		offset += length
+		return strings.Join(nameParts, "."), offset
+	}
+
+	// Check for the compression pointer (first two bits are 1s)
+	// Actual value will never be greater than 00111111, first 2 msb are used to indicate compression
+	// By doing bitwise & with 0xC0 (11000000) we can check if the first two bits are 1s if the results is 11000000
+	if length&0xC0 == 0xC0 {
+		// Read the offset of the compressed name, value is the loation of of the byte array to continue reading from
+		ptrOffset := int(binary.BigEndian.Uint16(data[offset:offset+2]) & 0x3FFF) // 0x3FF (1100000000000000) removes first 2 bits from 16 bit biary number
+		offset += 2                                                               //Move the main pointer forward after reading the compression poisition (2 bytes long)
+
+		// Recursively read the domain name from the pointer offset
+		compressedName, _ := readDomainName(data, ptrOffset)
+		fmt.Printf("compressedName:%s\n", compressedName)
+		nameParts = append(nameParts, compressedName)
+		return compressedName, offset
+	}
+
+	offset++
+	nameParts = append(nameParts, string(data[offset:offset+length]))
+	offset += length
+
+	if (offset + 1) <= len(data) {
+		nextName, nextOffset := readDomainName(data, offset)
+		offset = nextOffset
+		nameParts = append(nameParts, nextName)
 	}
 
 	return strings.Join(nameParts, "."), offset
@@ -362,6 +370,11 @@ func handleDNSRequest(conn *net.UDPConn, addr *net.UDPAddr, msg []byte) {
 	message := DNSMessage{}
 	message.Header = ParseHeader(msg)
 	fmt.Printf("Received DNS Query ID: %d\n", message.Header.ID)
+
+	if message.Header.Opcode > 2 {
+		fmt.Printf("  [%d] Opcode %d not supported\n", message.Header.ID, message.Header.Opcode)
+		return
+	}
 
 	// Parse the DNS question
 	offset := 12
@@ -495,8 +508,24 @@ func main() {
 			log.Printf("Failed to read DNS request: %v", err)
 			continue
 		}
+		logRequest(buffer[:n])
 
 		// Handle the DNS request in a separate goroutine
 		handleDNSRequest(conn, addr, buffer[:n])
+	}
+}
+
+func logRequest(buffer []byte) {
+	fi, err := os.OpenFile("request.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println("Failed to open file request.txt")
+		return
+	}
+	defer fi.Close()
+
+	_, err = fi.Write(buffer)
+	fi.WriteString("###")
+	if err != nil {
+		log.Println("Failed to write to request.txt")
 	}
 }
